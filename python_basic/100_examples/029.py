ad105443a750b785a92b10dcfcb833dff7ed7cd6
lijiale0625 <1052551319@qq.com>
2017-09-24 14:15:17 +0800

    031

--- python_basic/100_examples/029.py
+++ python_basic/100_examples/029.py
@@ -3,10 +3,24 @@
 # 程序分析：学会分解出每一位数。
 
 i = str(raw_input('请输入一个不多于5位的正整数：'))
-l = len(i)
 while (1):
-    if l > 5:
+    if len(i) > 5 or int(i) <= 0 :
         i = str(raw_input('输入有误，请重新输入：'))
-        l = len(i)
-    break
-print l
+    else:
+        print '%d的位数为%d'%(int(i),len(i))
+        print('逆序为：\n')
+        print(i[::-1])
+        break
+
+#参考
+# print( '请输入大于10的数字:' )
+# n=input()
+# x=[]
+# i=0;
+# while(n!=0):
+#     x.append(n%10)
+#     i+=1
+#     n/=10
+# print( '该数有 %d 位\n' %i )
+# print( '逆序为：\n')
+# print( x[::] )
