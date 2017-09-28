b267675fbe1941334d9384ebc6499d25a416995d
lijiale0625 <1052551319@qq.com>
2017-09-28 18:44:36 +0800

    @property

--- liaoxuefeng/property_demo1.py
+++ liaoxuefeng/property_demo1.py
@@ -37,3 +37,31 @@ f.set_fee("1")
 # print f.fee
 print f.get_fee()
 #Decimal('2')
+
+class Cls(object):
+    def __init__(self):
+        self.__x = None
+
+    @property
+    def x(self):
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        self.__x = value
+
+    @x.deleter
+    def x(self):
+        # pass
+        del self.__x
+
+
+if __name__ == '__main__':
+    c = Cls()
+    c.x = 200
+    y = c.x
+    print("set & get y: %d" % y)
+
+    del c.x
+    # print c.x #删除了c.x，不存在了，
+    print("del c.x & y: %d" % y)
